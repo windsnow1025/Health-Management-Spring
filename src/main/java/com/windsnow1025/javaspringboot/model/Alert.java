b437d4176e8be49a78015c03a9b65a885b740e2d
windsnow1025 <windsnow1025@gmail.com>
2023-11-29 04:13:11 +0000

    models

--- src/main/java/com/windsnow1025/javaspringboot/model/Alert.java
+++ src/main/java/com/windsnow1025/javaspringboot/model/Alert.java
@@ -1,5 +1,110 @@
 package com.windsnow1025.javaspringboot.model;
 
-public class Alert {
+import java.sql.Blob;
+import java.sql.Date;
 
+public class Alert {
+    String phone_number;
+    String alert_No;
+    int type_No;
+    String type;
+    Blob content;
+    String title;
+    Date date;
+    String cycle;
+    String is_medicine;
+    String is_deleted;
+
+    public Alert(String phone_number, String alert_No, int type_No, String type, Blob content, String title, Date date, String cycle, String is_medicine, String is_deleted) {
+        this.phone_number = phone_number;
+        this.alert_No = alert_No;
+        this.type_No = type_No;
+        this.type = type;
+        this.content = content;
+        this.title = title;
+        this.date = date;
+        this.cycle = cycle;
+        this.is_medicine = is_medicine;
+        this.is_deleted = is_deleted;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getAlert_No() {
+        return alert_No;
+    }
+
+    public void setAlert_No(String alert_No) {
+        this.alert_No = alert_No;
+    }
+
+    public int getType_No() {
+        return type_No;
+    }
+
+    public void setType_No(int type_No) {
+        this.type_No = type_No;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Blob getContent() {
+        return content;
+    }
+
+    public void setContent(Blob content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCycle() {
+        return cycle;
+    }
+
+    public void setCycle(String cycle) {
+        this.cycle = cycle;
+    }
+
+    public String getIs_medicine() {
+        return is_medicine;
+    }
+
+    public void setIs_medicine(String is_medicine) {
+        this.is_medicine = is_medicine;
+    }
+
+    public String getIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(String is_deleted) {
+        this.is_deleted = is_deleted;
+    }
 }
--- src/main/java/com/windsnow1025/javaspringboot/model/Report.java
+++ src/main/java/com/windsnow1025/javaspringboot/model/Report.java
@@ -1,4 +1,90 @@
 package com.windsnow1025.javaspringboot.model;
 
+import java.sql.Blob;
+import java.sql.Date;
+
 public class Report {
+    String phone_number;
+    int report_No;
+    Blob report_content;
+    Blob report_picture;
+    String report_type;
+    Date report_date;
+    String report_place;
+    String is_deleted;
+
+    public Report(String phone_number, int report_No, Blob report_content, Blob report_picture, String report_type, Date report_date, String report_place, String is_deleted) {
+        this.phone_number = phone_number;
+        this.report_No = report_No;
+        this.report_content = report_content;
+        this.report_picture = report_picture;
+        this.report_type = report_type;
+        this.report_date = report_date;
+        this.report_place = report_place;
+        this.is_deleted = is_deleted;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getReport_No() {
+        return report_No;
+    }
+
+    public void setReport_No(int report_No) {
+        this.report_No = report_No;
+    }
+
+    public Blob getReport_content() {
+        return report_content;
+    }
+
+    public void setReport_content(Blob report_content) {
+        this.report_content = report_content;
+    }
+
+    public Blob getReport_picture() {
+        return report_picture;
+    }
+
+    public void setReport_picture(Blob report_picture) {
+        this.report_picture = report_picture;
+    }
+
+    public String getReport_type() {
+        return report_type;
+    }
+
+    public void setReport_type(String report_type) {
+        this.report_type = report_type;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
+
+    public String getReport_place() {
+        return report_place;
+    }
+
+    public void setReport_place(String report_place) {
+        this.report_place = report_place;
+    }
+
+    public String getIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(String is_deleted) {
+        this.is_deleted = is_deleted;
+    }
 }
